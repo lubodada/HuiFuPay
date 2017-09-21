ed37577624deb9b776da02445f335bede2b09733
卢波 <1453662310@qq.com>
2017-09-21 16:11:33 +0800

    SecondBranch测试-----二维码

--- app/src/main/java/com/mining/app/zxing/image/QRImage.java
+++ app/src/main/java/com/mining/app/zxing/image/QRImage.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
@@ -14,71 +15,121 @@ import android.graphics.PorterDuff.Mode;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 public class QRImage {
-	private static int QR_WIDTH = 300;
-	private static int QR_HEIGHT = 300;
+    private static int QR_WIDTH = 300;
+    private static int QR_HEIGHT = 300;
+    private static final int IMAGE_HALFWIDTH = 40;//宽度值，影响中间图片大小
 
-	// 生成二维码图片
-	public static Bitmap createQRImage(String url) {
-		try {
-			// 判断URL合法性
-			if (url == null || "".equals(url) || url.length() < 1) {
-				return null;
-			}
-			Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
-			hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+    // 生成二维码图片
+    public static Bitmap createQRImage(String url) {
+        try {
+            // 判断URL合法性
+            if (url == null || "".equals(url) || url.length() < 1) {
+                return null;
+            }
+            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
 
-			BitMatrix bitMatrix = new QRCodeWriter().encode(url,
-					BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT);
-			int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
+            BitMatrix bitMatrix = new QRCodeWriter().encode(url,
+                    BarcodeFormat.QR_CODE, QR_WIDTH, QR_HEIGHT);
+            int[] pixels = new int[QR_WIDTH * QR_HEIGHT];
 
-			for (int y = 0; y < QR_HEIGHT; y++) {
-				for (int x = 0; x < QR_WIDTH; x++) {
-					if (bitMatrix.get(x, y)) {
-						pixels[y * QR_WIDTH + x] = 0xff000000;
-					} else {
-						pixels[y * QR_WIDTH + x] = 0xffffffff;
-					}
-				}
-			}
-			Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT,
-					Bitmap.Config.ARGB_8888);
-			bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
-			return bitmap;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+            for (int y = 0; y < QR_HEIGHT; y++) {
+                for (int x = 0; x < QR_WIDTH; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * QR_WIDTH + x] = 0xff000000;
+                    } else {
+                        pixels[y * QR_WIDTH + x] = 0xffffffff;
+                    }
+                }
+            }
+            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_HEIGHT,
+                    Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_HEIGHT);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	// 生成圆角图片
-	public static Bitmap GetRoundedCornerBitmap(Bitmap bitmap) {
-		try {
-			Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-					bitmap.getHeight(), Config.ARGB_8888);
-			Canvas canvas = new Canvas(output);
-			final Paint paint = new Paint();
-			final Rect rect = new Rect(0, 0, bitmap.getWidth(),
-					bitmap.getHeight());
-			final RectF rectF = new RectF(new Rect(0, 0, bitmap.getWidth(),
-					bitmap.getHeight()));
-			final float roundPx = 15;
-			paint.setAntiAlias(true);
-			canvas.drawARGB(0, 0, 0, 0);
-			paint.setColor(Color.BLACK);
-			canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-			paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+    // 生成圆角图片
+    public static Bitmap GetRoundedCornerBitmap(Bitmap bitmap) {
+        try {
+            Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                    bitmap.getHeight(), Config.ARGB_8888);
+            Canvas canvas = new Canvas(output);
+            final Paint paint = new Paint();
+            final Rect rect = new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight());
+            final RectF rectF = new RectF(new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight()));
+            final float roundPx = 15;
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            paint.setColor(Color.BLACK);
+            canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+            paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
 
-			final Rect src = new Rect(0, 0, bitmap.getWidth(),
-					bitmap.getHeight());
+            final Rect src = new Rect(0, 0, bitmap.getWidth(),
+                    bitmap.getHeight());
 
-			canvas.drawBitmap(bitmap, src, rect, paint);
-			return output;
-		} catch (Exception e) {
-			return bitmap;
-		}
-	}
+            canvas.drawBitmap(bitmap, src, rect, paint);
+            return output;
+        } catch (Exception e) {
+            return bitmap;
+        }
+    }
+
+    /**
+     *  生成二维码
+     * * @param string 二维码中包含的文本信息
+     * * @param mBitmap logo图片
+     * * @param format 编码格式
+     * * @return Bitmap 位图
+     * * @throws WriterException
+     *
+     */
+    public static Bitmap createCode(String string, Bitmap mBitmap, BarcodeFormat format)
+            throws WriterException {
+        Matrix m = new Matrix();
+        float sx = (float) 2 * IMAGE_HALFWIDTH / mBitmap.getWidth();
+        float sy = (float) 2 * IMAGE_HALFWIDTH / mBitmap.getHeight();
+        m.setScale(sx, sy);//设置缩放信息
+        //将logo图片按martix设置的信息缩放
+        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0,
+                mBitmap.getWidth(), mBitmap.getHeight(), m, false);
+        MultiFormatWriter writer = new MultiFormatWriter();
+        Hashtable hst = new Hashtable();
+        hst.put(EncodeHintType.CHARACTER_SET, "UTF-8");//设置字符编码
+        BitMatrix matrix = writer.encode(string, format, 400, 400, hst);//生成二维码矩阵信息
+        int width = matrix.getWidth();//矩阵高度
+        int height = matrix.getHeight();//矩阵宽度
+        int halfW = width / 2;
+        int halfH = height / 2;
+        int[] pixels = new int[width * height];//定义数组长度为矩阵高度*矩阵宽度，用于记录矩阵中像素信息
+        for (int y = 0; y < height; y++) {//从行开始迭代矩阵
+            for (int x = 0; x < width; x++) {//迭代列
+                if (x > halfW - IMAGE_HALFWIDTH && x < halfW + IMAGE_HALFWIDTH
+                        && y > halfH - IMAGE_HALFWIDTH
+                        && y < halfH + IMAGE_HALFWIDTH) {//该位置用于存放图片信息
+                    //记录图片每个像素信息
+                    pixels[y * width + x] = mBitmap.getPixel(x - halfW + IMAGE_HALFWIDTH, y - halfH + IMAGE_HALFWIDTH);
+                } else {
+                    if (matrix.get(x, y)) {//如果有黑块点，记录信息
+                        pixels[y * width + x] = 0xff000000;//记录黑块信息
+                    }
+                }
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        // 通过像素数组生成bitmap
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
 }
